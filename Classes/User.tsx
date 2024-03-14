dd7a619f5ed181adfb310ec9fac8d0d2b0d110df
AndresMarquez <90575901+aemr-LL7@users.noreply.github.com>
2024-03-13 22:48:05 -0400

    Delete Classes directory

--- Classes/User.tsx
+++
@@ -1,8 +0,0 @@
-export default interface Usuario {
-  username: string;
-  email: string;
-  getName(): string;
-  setName(newName: string): void;
-  getEmail(): string;
-  setEmail(newEmail: string): void;
-}
